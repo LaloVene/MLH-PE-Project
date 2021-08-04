2837ae75ddcd2b42a298bcb829d84e4f623ef298
Eduardo Venegas <lalohdez77@gmail.com>
2021-08-03 21:30:19 -0500

    Finish register and login endpoints

--- api/api.py
+++ api/api.py
@@ -34,7 +34,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 class UserModel(db.Model):
     __tablename__ = "users"
 
@@ -54,6 +53,30 @@ class UserModel(db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
+class LanguageModel(db.Model):
+    __tablename__ = "language"
+
+    name = db.Column(db.String(), primary_key=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"<Language {self.name}>"
+class RelUserLanguage(db.Model):
+    __tablename__ = "relUserLanguage"
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    username = db.Column(db.String())
+    langName = db.Column(db.String())
+
+    def __init__(self, username, langName):
+        self.username = username
+        self.langName = langName
+
+    def __repr__(self):
+        return f"<RelUserLanguage {self.langName}>"
+
 
 @app.errorhandler(404)
 def not_found(e):
@@ -69,15 +92,19 @@ def register():
         body = request.get_json()
         username = str(body['username'])
         password = str(body['password'])
+        name = str(body['name'])
+        email = str(body['email'])
+        github = str(body['github'])
         error = None
 
-        if not username or not password:
+        if (not username or not password or not name or not email or not github):
             error = "Missing Data"
+            return jsonify({"status": "1"}), 400
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered"
 
         if error is None:
-            new_user = UserModel(username, generate_password_hash(password))
+            new_user = UserModel(username, generate_password_hash(password), name, email, github)
             db.session.add(new_user)
             db.session.commit()
             message = f"User {username} created successfully"
@@ -110,6 +137,44 @@ def login():
 
     except:
         return jsonify({"status": "bad", "message": "missing or invalid data"}), 400
+
+@app.route('/api/addLanguage', methods=("POST",))
+def addLanguage():
+    try:
+        body = request.get_json()
+        username = str(body['username'])
+        language = str(body['language'])
+        error = None
+
+        if not username or not language:
+            error = "Missing Data"
+
+        if error is None:
+            add_language = RelUserLanguage(username, language)
+            db.session.add(add_language)
+            db.session.commit()
+            message = f"User {language} added to user {username} successfully"
+            return jsonify({"status": "ok", "message": message}), 200
+        else:
+            return jsonify({"status": "bad", "message": "missing or invalid data", "error": error}), 400
+
+    except:
+        return jsonify({"status": "bad", "message": "missing or invalid data"}), 400
+
+@app.route('/api/getUserLanguages', methods=("POST",))
+def getUserLanguages():
+    body = request.get_json()
+    username = str(body['username'])
+    error = None
+
+    if not username:
+        error = "Missing Data"
+
+    if error is None:
+        languages = RelUserLanguage.query.filter_by(username=username).all()
+        return jsonify(json_list = languages)
+    else:
+        return jsonify({"status": "bad", "message": "missing or invalid data", "error": error}), 400
 
 @app.route('/api/time')
 def get_current_time():
