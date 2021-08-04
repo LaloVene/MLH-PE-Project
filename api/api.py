c60817b5f456279ae276f0567257cdadefb683a2
Eduardo Venegas <lalohdez77@gmail.com>
2021-08-04 11:50:32 -0500

    Added Topics endpoints

--- api/api.py
+++ api/api.py
@@ -34,6 +34,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+#--------------- USER MODEL------------------
 class UserModel(db.Model):
     __tablename__ = "users"
 
@@ -49,10 +50,10 @@ class UserModel(db.Model):
         self.name = name
         self.email = email
         self.github = github
-
     def __repr__(self):
         return f"<User {self.username}>"
 
+#--------------- LANGUAGE MODEL------------------
 class LanguageModel(db.Model):
     __tablename__ = "language"
 
@@ -60,18 +61,9 @@ class LanguageModel(db.Model):
 
     def __init__(self, name):
         self.name = name
-
-    @property
-    def get(self):
-       return {
-        'name' : self.name,
-       }
-    @property
-    def getAll(self, languages):
-       return [ item.get for item in languages]
-
     def __repr__(self):
         return f"<Language {self.name}>"
+
 class RelUserLanguage(db.Model):
     __tablename__ = "relUserLanguage"
 
@@ -82,9 +74,100 @@ class RelUserLanguage(db.Model):
     def __init__(self, username, langName):
         self.username = username
         self.langName = langName
-
     def __repr__(self):
-        return f"<RelUserLanguage {self.langName}>"
+        return f"<RelUserLanguage {self.username} {self.langName}>"
+
+#--------------- TOPIC MODEL------------------
+class TopicModel(db.Model):
+    __tablename__ = "topic"
+
+    name = db.Column(db.String(), primary_key=True)
+
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return f"<Topic {self.name}>"
+
+class RelUserTopic(db.Model):
+    __tablename__ = "relUserTopic"
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    username = db.Column(db.String())
+    topicName = db.Column(db.String())
+
+    def __init__(self, username, topicName):
+        self.username = username
+        self.topicName = topicName
+    def __repr__(self):
+        return f"<RelUserTopic {self.username} {self.topicName}>"
+
+#--------------- PROJECT MODEL------------------
+class ProjectModel(db.Model):
+    __tablename__ = "project"
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    title = db.Column(db.String())
+    description = db.Column(db.Text())
+    url = db.Column(db.String())
+
+    def __init__(self, title, description, url):
+        self.title = title
+        self.description = description
+        self.url = url
+    def __repr__(self):
+        return f"<Project {self.title}>"
+
+class RelUserInProject(db.Model):
+    __tablename__ = "relUserInProject"
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    username = db.Column(db.String())
+    projectId = db.Column(db.Integer())
+
+    def __init__(self, username, projectId):
+        self.username = username
+        self.projectId = projectId
+    def __repr__(self):
+        return f"<RelUserInProject {self.username} {self.projectId}>"
+        
+class RelUserFavProject(db.Model):
+    __tablename__ = "relUserFavProject"
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    username = db.Column(db.String())
+    projectId = db.Column(db.Integer())
+
+    def __init__(self, username, projectId):
+        self.username = username
+        self.projectId = projectId
+    def __repr__(self):
+        return f"<RelUserFavProject {self.username} {self.projectId}>"
+
+class RelProjectLanguage(db.Model):
+    __tablename__ = "relProjectLanguage"
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    language = db.Column(db.String())
+    projectId = db.Column(db.Integer())
+
+    def __init__(self, language, projectId):
+        self.language = language
+        self.projectId = projectId
+    def __repr__(self):
+        return f"<RelProjectLanguage {self.language} {self.projectId}>"
+
+class RelProjectTopic(db.Model):
+    __tablename__ = "relProjectTopic"
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    topic = db.Column(db.String())
+    projectId = db.Column(db.Integer())
+
+    def __init__(self, topic, projectId):
+        self.topic = topic
+        self.projectId = projectId
+    def __repr__(self):
+        return f"<RelProjectTopic {self.topic} {self.projectId}>"
 
 
 @app.errorhandler(404)
@@ -150,8 +233,143 @@ def login():
         return jsonify({"status": "bad", "error": "missing or invalid data"}), 400
 
 
-########### LANGUAGES ##############
+########### TOPICS ##############
 
+@app.route('/api/addTopic', methods=("POST",))
+def addTopic():
+    try:
+        body = request.get_json()
+        topic = str(body['topic'])
+        error = None
+
+        if not topic:
+            error = "Missing Data"
+        elif TopicModel.query.filter_by(name=topic).first() is not None:
+            error = f"Topic {topic} already exists"
+
+        if error is None:
+            add_topic = TopicModel(topic)
+            db.session.add(add_topic)
+            db.session.commit()
+            message = f"Added topic {topic} successfully"
+            return jsonify({"status": "ok", "message": message}), 200
+        else:
+            return jsonify({"status": "bad", "error": error}), 400
+
+    except:
+        return jsonify({"status": "bad", "error": "missing or invalid data"}), 400
+
+@app.route('/api/getTopics', methods=("GET",))
+def getTopics():
+    try:
+        response = TopicModel.query.all()
+        topics = []
+        for item in response:
+            topics.append({
+                "name": item.name
+                })
+        return jsonify({"topics": topics}), 200
+    except:
+        return jsonify({"status": "bad", "error": "missing or invalid data"}), 400
+
+@app.route('/api/deleteTopic', methods=("DELETE",))
+def deleteTopic():
+    try:
+        body = request.get_json()
+        topic = str(body['topic'])
+        error = None
+
+        if not topic:
+            error = "Missing Data"
+        elif TopicModel.query.filter_by(name=topic).first() is None:
+            error = f"No topic {topic}"
+
+        if error is None:
+            TopicModel.query.filter_by(name=topic).delete()
+            db.session.commit()
+            message = f"topic with name {topic} removed"
+            return jsonify({"status": "ok", "message": message}), 200
+        else:
+            return jsonify({"status": "bad", "error": error}), 400
+
+    except:
+        return jsonify({"status": "bad", "error": "missing or invalid data"}), 400
+
+########### USER TOPICS ##############
+
+@app.route('/api/addUserTopic', methods=("POST",))
+def addUserTopic():
+    try:
+        body = request.get_json()
+        username = str(body['username'])
+        topic = str(body['topic'])
+        error = None
+
+        if not username or not topic:
+            error = "Missing Data"
+        elif RelUserTopic.query.filter_by(username=username, topicName=topic).first() is not None:
+            error = f"User {username} already has topic {topic}"
+
+        if error is None:
+            add_topic = RelUserTopic(username, topic)
+            db.session.add(add_topic)
+            db.session.commit()
+            message = f"User {topic} added to user {username} successfully"
+            return jsonify({"status": "ok", "message": message}), 200
+        else:
+            return jsonify({"status": "bad", "error": error}), 400
+
+    except:
+        return jsonify({"status": "bad", "error": "missing or invalid data"}), 400
+
+@app.route('/api/getUserTopics', methods=("POST",))
+def getUserTopics():
+    try:
+        body = request.get_json()
+        username = str(body['username'])
+        error = None
+
+        if not username:
+            error = "Missing Data"
+
+        if error is None:
+            response = RelUserTopic.query.filter_by(username=username).all()
+            topics = []
+            for item in response:
+                topics.append({
+                    "id": item.id,
+                    "name": item.topicName
+                    })
+            return jsonify({"topics": topics}), 200
+        else:
+            return jsonify({"status": "bad", "error": error}), 400
+    except:
+        return jsonify({"status": "bad", "error": "missing or invalid data"}), 400
+
+@app.route('/api/deleteUserTopic', methods=("DELETE",))
+def deleteUserTopic():
+    try:
+        body = request.get_json()
+        id = str(body['id'])
+        error = None
+
+        if not id:
+            error = "Missing Data"
+        elif RelUserTopic.query.filter_by(id=id).first() is None:
+            error = f"User have no topic with id {id}"
+
+        if error is None:
+            RelUserTopic.query.filter_by(id=id).delete()
+            db.session.commit()
+            message = f"Topic with id {id} removed"
+            return jsonify({"status": "ok", "message": message}), 200
+        else:
+            return jsonify({"status": "bad", "error": error}), 400
+
+    except:
+        return jsonify({"status": "bad", "error": "missing or invalid data"}), 400
+
+########### LANGUAGES ##############
 @app.route('/api/addLanguage', methods=("POST",))
 def addLanguage():
     try:
