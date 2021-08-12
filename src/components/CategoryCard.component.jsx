a5ebd8e2c238c30c671842b7a3b56327eed6c9fb
= <wangela472@gmail.com>
2021-08-12 16:19:09 -0400

    Update categories card UI

--- src/components/CategoryCard.component.jsx
+++ src/components/CategoryCard.component.jsx
@@ -15,14 +15,10 @@ const Card = styled(IonCard)`
   cursor: pointer;
   text-align: center;
   border-radius: 1rem;
-  height: 10rem;
-  /* width: 10rem; */
+  height: 11rem;
   background-color: #ffe1be;
-  /* box-shadow: 0 0.3px 2.2px rgba(0, 0, 0, 0.02),
-  0 0.8px 5.3px rgba(0, 0, 0, 0.028), 0 1.5px 10px rgba(0, 0, 0, 0.035),
-  0 2.7px 17.9px rgba(0, 0, 0, 0.042), 0 5px 33.4px rgba(0, 0, 0, 0.05),
-  0 12px 80px rgba(0, 0, 0, 0.07); */
   box-shadow: none;
+  max-width: 20vw;
   
   &:hover {
     border: 4px solid hsl(32.14285714285714, 59.15492957746478%, 72.15686274509804%);
@@ -31,10 +27,11 @@ const Card = styled(IonCard)`
 
 const Icon = styled(IonIcon)`
   font-size: 3rem;
+  margin-bottom: 1rem;
 `;
 
 const Title = styled(IonCardTitle)`
-  font-size: 1.2rem;
+  font-size: 1.1rem;
 `;
 
 function CategoryCard(props) {
