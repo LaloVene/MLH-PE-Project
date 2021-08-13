3b3e31e5282131965c1a42976b228c04c2f02025
= <wangela472@gmail.com>
2021-08-13 16:58:07 -0400

    Add ability to add languages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,15 @@
-import { IonContent, IonGrid, IonPage, IonRow } from '@ionic/react';
+import { IonContent, useIonAlert, IonGrid, IonPage, IonRow, IonIcon, IonButton, IonModal, IonSelect, IonSelectOption } from '@ionic/react';
+import {
+  Icon,
+  TagTitle,
+  TitleInput,
+  DescriptionInput,
+  LinkInput,
+  ModalContent,
+  ModalContentView,
+  ButtonsWrapper,
+  SmallIcon
+} from '../components/ProjectCardStyles';
 import React, { useEffect, useState, useContext } from 'react';
 import styled from 'styled-components'
 import ProjectCard from "../components/ProjectCard.component";
@@ -8,6 +19,10 @@ import Header from '../components/Header.component';
 import NotFound from '../components/NotFound.component';
 import GlobalContext from "../utils/state/GlobalContext";
 import { useJwt } from "react-jwt";
+import { pencilOutline, close, checkmark } from 'ionicons/icons';
+// import { EditProfileDetails } from '../components/EditLanguagesInterests';
+import dbtopics from "../utils/topics.json";
+import dblanguages from "../utils/languages.json";
 
 const Title = styled.h4`
     margin-bottom: 12px;
@@ -33,15 +48,116 @@ const TagSection = styled.div`
     text-align: center;
 `
 
+const EditIcon = styled.button`
+  background-color: rgba(0,0,0,0);
+  color: black;
+  font-size: 1.1rem;
+  &:hover {
+    color: grey;
+  }
+`;
+
+
+
 function ProfilePage() {
 
   // Auth
-  const {state} = useContext(GlobalContext);
+  const { state } = useContext(GlobalContext);
   const { decodedToken } = useJwt(state.token);
 
   const [profileData, setProfileData] = useState([]);
+  const [profileLanguages, setProfileLanguages] = useState([]);
   const [projectList, setProjectList] = useState([]);
 
+  const [editLanguagesDetails, setEditLanguagesDetails] = useState(false);
+  const [editInterestsDetails, setEditInterestsDetails] = useState(false);
+
+  const [present] = useIonAlert();
+
+  function saveChanges() {
+    console.log('pressed save')
+    if (editLanguagesDetails) {
+      if (!profileLanguages) {
+        return present({
+          header: "Please select at least one option!",
+          buttons: [
+            'Ok'
+          ]
+        })
+      } else {
+
+        profileLanguages.forEach(function (language) {
+          fetch('/api/addUserLanguage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              'username': profileData.username,
+              'language': language
+            })
+          }).then(r => r.json())
+            .then(resp => {
+
+              console.log(language)
+              if (resp.status === "ok") {
+                console.log(resp.message)
+              }
+              else {
+                console.log(resp.error)
+              }
+            })
+        })
+        return present({
+          header: "Languages added!",
+          buttons: [
+            {
+              text: 'Ok', handler: (d) => {
+                setEditLanguagesDetails(false)
+              }
+            }
+          ]
+        })
+
+      }
+    }
+  }
+  function EditProfileDetails() {
+    return (
+      <>
+        <IonModal isOpen={editLanguagesDetails || editInterestsDetails}>
+          <ModalContent>
+            <TagTitle>Edit Section</TagTitle>
+            {editLanguagesDetails ? <IonSelect
+              style={{ height: "40px", width: "500px", marginLeft: "20px" }}
+              value={profileLanguages}
+              multiple={true}
+              cancelText="Close"
+              okText="Done"
+              placeholder="Select language(s)"
+              onIonChange={e => (setProfileLanguages(e.target.value))}>
+              {
+                dblanguages.map(topic =>
+                  <IonSelectOption key={topic} value={topic}>{topic}</IonSelectOption>
+                )
+              }
+            </IonSelect> : <></>}
+
+            <ButtonsWrapper>
+              <IonButton color="success" id="closemodal" onClick={saveChanges}  >
+                <SmallIcon slot="start" icon={checkmark} />
+                Save
+              </IonButton>
+              <IonButton style={{ marginBottom: "50px" }} id="closemodal" onClick={() => { setEditLanguagesDetails(false); setEditInterestsDetails(false)}}>
+                <SmallIcon slot="start" icon={close} />
+                Close
+              </IonButton>
+            </ButtonsWrapper>
+          </ModalContent>
+        </IonModal>
+      </>
+    )
+  }
   useEffect(() => {
     if (decodedToken) {
       fetch(`/api/getUserData?username=${decodedToken.username}`).then(res => res.json()).then(data => {
@@ -59,9 +175,17 @@ function ProfilePage() {
 
   const placeholderBio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 
+  function EditLanguages() {
+    console.log(profileData.languages);
+    console.log()
+  }
+  function EditInterests() {
+    console.log('heaallo');
+    console.log()
+  }
   return (
     <IonPage>
-      <Header/>
+      <Header />
       <IonContent>
         <Wrapper>
 
@@ -72,7 +196,15 @@ function ProfilePage() {
           <IonRow>
             <Section>
               <Title>
+                &nbsp;&nbsp;
                 Languages
+                &nbsp;
+                <EditIcon
+                  onClick={() => setEditLanguagesDetails(true)}
+                >
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </EditIcon>
+                <EditProfileDetails />
               </Title>
               <TagSection>
                 {profileData.languages ? profileData.languages.map(language => <Tag key={language.name} text={language.name} />) : <div />}
@@ -80,7 +212,14 @@ function ProfilePage() {
             </Section>
             <Section>
               <Title>
+                &nbsp;&nbsp;
                 Interests
+                &nbsp;
+                <EditIcon
+                  onClick={EditInterests}
+                >
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </EditIcon>
               </Title>
               <TagSection>
                 {profileData.topics ? profileData.topics.map(topic => <Tag key={topic.name} text={topic.name} />) : <div />}
@@ -93,8 +232,8 @@ function ProfilePage() {
             Projects
           </Title>
           <IonGrid>
-              {
-                projectList.filter(project => project.owner === profileData.username).length ?
+            {
+              projectList.filter(project => project.owner === profileData.username).length ?
                 <IonRow>
                   {projectList.filter(project => project.owner === profileData.username).map(project => {
                     const { id, title, description, date, url, owner } = project;
@@ -113,8 +252,8 @@ function ProfilePage() {
                   })}
                 </IonRow>
                 :
-                  <NotFound title="No Projects" message="You don't have projects yet"/>
-              }
+                <NotFound title="No Projects" message="You don't have projects yet" />
+            }
           </IonGrid>
 
         </Wrapper>
