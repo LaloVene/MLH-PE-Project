e6459f87b423f2587fb4ee1d8680cec184ddea31
= <wangela472@gmail.com>
2021-08-13 14:31:02 -0400

    Limit number of characters in contact message

--- src/components/ProjectCard.component.jsx
+++ src/components/ProjectCard.component.jsx
@@ -1,15 +1,9 @@
-import React, { useState, useContext,useEffect } from "react";
+import { useState, useContext } from "react";
 import {
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardSubtitle,
   IonCardContent,
-  IonIcon,
   IonCol,
   IonModal,
   IonButton,
-  IonItem,
   useIonAlert
 } from "@ionic/react";
 import {
@@ -23,46 +17,31 @@ import {
   Description,
   ProjTitle,
   Owner,
-  TagText,
-  TagTitle,
   TitleInput,
   DescriptionInput,
-  LinkInput,
-  TagsWrapper,
   ModalContent,
   ModalContentView,
   ButtonsWrapper
 } from './ProjectCardStyles';
 import ProjectTags from './ProjectTags';
-import { personCircleOutline, send, close, open } from "ionicons/icons";
-import styled from "styled-components";
+import { personCircleOutline, send, close, open, mail } from "ionicons/icons";
 import { useJwt } from "react-jwt";
 import GlobalContext from "../utils/state/GlobalContext";
 import { LRButton } from '../components/LRStyles'
 
-const Tags = styled.p`
-  padding-top: 1rem;
-  color: #B4B5B8;
-`;
-
 function CategoryCard(props) {
 
-  const { title, description, date, url, owner, id,languages,topics} = props;
+  const { title, description, date, url, owner, id, languages, topics } = props;
   const [showProject, setShowProject] = useState(false);
   const [showContact, setShowContact] = useState(false);
   const [mTitle, setMTitle] = useState("");
   const [mMessage, setMMessage] = useState("");
-
-
-  
-  
 
   const { state } = useContext(GlobalContext);
   const { decodedToken } = useJwt(state.token);
   const [present] = useIonAlert()
 
   function sendEmail() {
-
 
     if (!mTitle || !mMessage) {
       return present({
@@ -78,6 +57,7 @@ function CategoryCard(props) {
           'message': mMessage,
           // email of receiver, username of sender
           'sender': decodedToken.username,
+          'senderEmail': decodedToken.email,
           'receiver': data.userData.email
         }
         return fetch('/api/sendmessage', {
@@ -90,14 +70,12 @@ function CategoryCard(props) {
       })
 
         .then(r => r.json())
-        .then(resp => {
-
-        }).then(() => {
+        .then(() => {
           return present({
             header: "Message sent!",
             buttons: [
               {
-                text: 'Ok', handler: (d) => {
+                text: 'Ok', handler: () => {
                   setShowContact(false)
                   setMTitle("")
                   setMMessage("")
@@ -109,97 +87,93 @@ function CategoryCard(props) {
     }
   }
 
+  return (
+    <IonCol size="12" size-md="4" key={id}>
+      {!showContact &&
+        <IonModal id="projmod" isOpen={showProject}>
+          <ModalContent>
+            <ModalContentView>
+              <ProjTitle >{title}</ProjTitle>
+              <Owner>Created By: {owner}</Owner>
+              <Date>{date}</Date>
+              <Description >{description}</Description>
+              <LRButton onClick={() => {
+                const fullURL = url.match(/^https?:/) ? url : '//' + url
+                window.open(fullURL)
+              }}>
+                <SmallIcon slot="start" icon={open} />
+                More Information
+              </LRButton>
 
-    return (
-      <IonCol size="12" size-md="4" key={id}>
-        {!showContact &&
-          <IonModal id="projmod" isOpen={showProject}>
-            <ModalContent>
-              <ModalContentView>
-                <ProjTitle >{title}</ProjTitle>
-                <Owner>Created By: {owner}</Owner>
-                <Date>{date}</Date>
-                <Description >{description}</Description>
-                <LRButton onClick={() => {
-                  const fullURL = url.match(/^https?:/) ? url : '//' + url
-                  window.open(fullURL)
-                }}>
-                  <SmallIcon slot="start" icon={open} />
-                  More Information
-                </LRButton>
-  
-                <ButtonsWrapper>
-                  <IonButton id="closemodal" color="tertiary" onClick={() => setShowContact(true)}>
-                    <SmallIcon slot="start" icon={send} />
-                    Contact
-                  </IonButton>
-                  <IonButton style={{ marginBottom: "50px" }} id="closemodal" onClick={() => setShowProject(false)}>
-                    <SmallIcon slot="start" icon={close} />
-                    Close
-                  </IonButton>
-                </ButtonsWrapper>
-  
-              </ModalContentView>
-            </ModalContent>
-          </IonModal>
-        }
-        {showContact &&
-          <IonModal id="projmod" isOpen={showProject}>
-            <ModalContent>
-  
-              <TitleInput
-                value={mTitle}
-                placeholder="Subject"
-                onIonChange={(e) => setMTitle(e.target.value)}
-              ></TitleInput>
-              <DescriptionInput
-                value={mMessage}
-                placeholder="Message"
-                onIonChange={(e) => setMMessage(e.target.value)}
-              ></DescriptionInput>
               <ButtonsWrapper>
-  
-                <IonButton id="closemodal" color="secondary" onClick={sendEmail}>
-                  <SmallIcon slot="start" icon={send} />
-                  Send
+                <IonButton id="closemodal" color="tertiary" onClick={() => setShowContact(true)}>
+                  <SmallIcon slot="start" icon={mail} />
+                  Contact
                 </IonButton>
-                <IonButton
-                  style={{ marginBottom: "50px" }}
-                  id="closemodal"
-                  onClick={() => setShowContact(false)}
-                >
+                <IonButton style={{ marginBottom: "50px" }} id="closemodal" onClick={() => setShowProject(false)}>
+                  <SmallIcon slot="start" icon={close} />
                   Close
                 </IonButton>
               </ButtonsWrapper>
-  
-            </ModalContent>
-          </IonModal>
-        }
-  
-        <Card onClick={() => setShowProject(true)}>
-          <CardHeader>
-            <Icon icon={personCircleOutline} />
-            <Username>{owner}</Username>
-          </CardHeader>
-          <IonCardContent>
-            <Title>{title}</Title>
-            <Description>{description}</Description>
-            <Date style={{ textAlign: "right" }}>{date}</Date>
-  
-  
-            <ProjectTags title="Languages" tagType={languages} limit={true} />
-            <ProjectTags title="Tags" tagType={topics} limit={true} />
-  
-          </IonCardContent>
-        </Card>
-      </IonCol>
-    );
-//   }
-// else {
-//   return null
-// }
-}
 
-  
+            </ModalContentView>
+          </ModalContent>
+        </IonModal>
+      }
+      {showContact &&
+        <IonModal id="projmod" isOpen={showProject}>
+          <ModalContent>
+
+            <TitleInput
+              value={mTitle}
+              placeholder="Subject"
+              onIonChange={(e) => setMTitle(e.target.value)}
+              maxlength={30}
+            ></TitleInput>
+            <DescriptionInput
+              value={mMessage}
+              placeholder="Message"
+              onIonChange={(e) => setMMessage(e.target.value)}
+              rows={5}
+              maxlength={240}
+            ></DescriptionInput>
+            <ButtonsWrapper>
+
+              <IonButton id="closemodal" color="secondary" onClick={sendEmail}>
+                <SmallIcon slot="start" icon={send} />
+                Send
+              </IonButton>
+              <IonButton
+                style={{ marginBottom: "50px" }}
+                id="closemodal"
+                onClick={() => setShowContact(false)}
+              >
+                Close
+              </IonButton>
+            </ButtonsWrapper>
+
+          </ModalContent>
+        </IonModal>
+      }
+
+      <Card onClick={() => setShowProject(true)}>
+        <CardHeader>
+          <Icon icon={personCircleOutline} />
+          <Username>{owner}</Username>
+        </CardHeader>
+        <IonCardContent>
+          <Title>{title}</Title>
+          <Description>{description}</Description>
+          <Date style={{ textAlign: "right" }}>{date}</Date>
+
+
+          <ProjectTags title="Languages" tagType={languages} limit={true} />
+          <ProjectTags title="Tags" tagType={topics} limit={true} />
+
+        </IonCardContent>
+      </Card>
+    </IonCol>
+  );
+}
 
 export default CategoryCard;
