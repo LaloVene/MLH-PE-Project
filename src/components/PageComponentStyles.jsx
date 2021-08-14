51a6ebdaa16f6073d2cff32b872aa0103c68a279
= <wangela472@gmail.com>
2021-08-14 17:08:07 -0400

    Move styled components to separate file

---
+++ src/components/PageComponentStyles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+  max-width: 1200px;
+  margin: auto;
+  padding: 1rem;
+`;
+
+const SearchBarContainer = styled.h1`
+  max-width: 20rem;
+  font-size: 1rem;
+  padding-left: 1rem;
+`;
+
+export { PageContainer, SearchBarContainer};
