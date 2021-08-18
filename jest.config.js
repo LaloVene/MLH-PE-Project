beebc6bd025a9cfab302888cde3a9b11ba3e6213
= <wangela472@gmail.com>
2021-08-18 15:00:57 -0400

    Add configuration for png files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     verbose: true,
     moduleNameMapper: {
-        '\\.(css|less|scss|sass|svg)$': 'identity-obj-proxy'
+        '\\.(css|less|scss|sass|svg|png)$': 'identity-obj-proxy'
     },
     testEnvironment: 'jsdom',
     errorOnDeprecated: true,
