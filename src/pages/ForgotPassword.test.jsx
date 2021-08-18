37a06a4f714abc38cb634bf601f6d6571df9abe8
mshen63 <mshen63@gatech.edu>
2021-08-18 19:51:27 -0400

    ForgotPassword test

--- src/pages/ForgotPassword.test.jsx
+++ src/pages/ForgotPassword.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render,getByTestId } from '@testing-library/react';
 import ForgotPassword from './ForgotPassword.page';
 import GlobalContext from '../utils/state/GlobalContext';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 describe('<ForgotPassword />', () => {
   test('Title rendered', async () => {
@@ -9,7 +10,7 @@ describe('<ForgotPassword />', () => {
       state:{token:null},
       dispatch:jest.fn()
     }
-    const { getByText } = await render(<GlobalContext.Provider value={mockstate}><ForgotPassword /></GlobalContext.Provider>);
+    const { getByText } = await render(<Router><GlobalContext.Provider value={mockstate}><ForgotPassword /></GlobalContext.Provider></Router>);
     const pass = getByText('Forgot Password');
     expect(pass).toBeDefined();
   });
