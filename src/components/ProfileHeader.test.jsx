c689475a214e1d54e14c5acbb07933e536ef544e
= <wangela472@gmail.com>
2021-08-18 14:39:20 -0400

    Remove unused import

--- src/components/ProfileHeader.test.jsx
+++ src/components/ProfileHeader.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByTestId, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import ProfileHeader from './ProfileHeader.component';
 
