8d7883344969a6a70aaecd7c252a0b9476e817e6
= <wangela472@gmail.com>
2021-08-19 00:57:45 -0400

    Add initial testing to render ProjectCard component

---
+++ src/components/ProjectCard.test.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, fireEvent, getByTestId} from '@testing-library/react';
+
+import ProjectCard from './ProjectCard.component';
+import GlobalContext from '../utils/state/GlobalContext';
+
+describe('<ProjectCard />', () => {
+
+    const mockstate = {
+        state: { token: null },
+        dispatch: jest.fn()
+    }
+
+    const mockProjectCard = {
+        title: "Project Test",
+        description: "This is a test description",
+        date: "",
+        url: "https://github.com/LaloVene/MLH-PE-Project",
+        owner: "Test Owner",
+        id: 1,
+        editFunc: "",
+        languages: ["Python", "JavaScript"],
+        topics: ["Machine Learning", "Production Engineering"],
+        collabs: ["Test Collaborator"]
+    }
+
+    test('Project Card is rendered', async () => {
+
+        const { getByText } = await render(<GlobalContext.Provider value={mockstate}><ProjectCard {...mockProjectCard}></ProjectCard></GlobalContext.Provider>);
+
+        const cardTitle = getByText("Project Test")
+        const cardDescription = getByText("This is a test description")
+        const cardOwner = getByText("Test Owner")
+        const cardLanguages = getByText("Python", "JavaScript")
+        const cardTopics = getByText("Machine Learning" && "Production Engineering")
+        const cardCollabs = getByText("Test Collaborator")
+        
+        expect(cardTitle).toBeDefined();
+        expect(cardDescription).toBeDefined();
+        expect(cardOwner).toBeDefined();
+        expect(cardLanguages).toBeDefined();
+        expect(cardTopics).toBeDefined();
+        expect(cardCollabs).toBeDefined();
+    });
+});
